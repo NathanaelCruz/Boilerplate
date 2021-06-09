ac5086d22f68965f4ef741c25e13bf5bb9867605
Nathanael Cruz <nathanael.cruz.fd@gmail.com>
2021-06-09 19:32:07 -0300

    Adjust in configurations

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -6,13 +6,13 @@ const Main = ({
 }) => (
     <S.Wrapper>
         <S.Logo
-            src="/img/logo.svg"
+            src="img/logo.svg"
             alt="Logo: Imagem de um átomo e react avançado"
         />
         <S.Title>{title}</S.Title>
         <S.Description>{description}</S.Description>
         <S.Illustration
-            src="/img/hero-illustration.svg"
+            src="img/hero-illustration.svg"
             alt="Desenvolvedor codificando em frente a um computador."
         />
     </S.Wrapper>
